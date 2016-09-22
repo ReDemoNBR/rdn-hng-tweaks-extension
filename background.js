0fcd9beeac420f7b675b999d92bf20d75c57a33f
San <alanderson_laird@yahoo.com.br>
2016-09-21 23:22:54 -0300

    v0.2 fix, now scripts are injected when page finishes the loading

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 //fired when the DOM of heroesandgenerals.com is loaded
-chrome.webNavigation.onDOMContentLoaded.addListener(
+chrome.webNavigation.onCompleted.addListener(
 	(res)=>chrome.pageAction.show(res.tabId) || chrome.tabs.executeScript(res.tabId, {file: "inject/style.js"}) || chrome.tabs.executeScript(res.tabId, {file: "inject/subforum_precedence.js"}),
 	{url: [{urlContains: 'heroesandgenerals.com/'}]}
 );
